fb2c6e9f41e990377af7350195ffe010e5c91889
Kit Jonathan Te <kjonathante@gmail.com>
2018-08-26 16:39:30 -0700

    Convert and modify the following functions buildBodyWithVariables, buildHeaders, buildRequestOptions and makeRequest

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var fs = require('fs')
 var path = require('path')
-
-// const loadQuery = (fileName) => new Promise((res, reject) => {
-//     readFile(resolve(fileName), 'utf-8', (err, data) => {
-//         (err) ? reject(err) : res(data)
-//     })
-// })
-
+var https = require('https')
+require('dotenv').config()
 
 function loadQuery( filename ) {
   return new Promise( function( resolve, reject ) {
@@ -16,13 +11,95 @@ function loadQuery( filename ) {
   })
 }
 
-var x = async function () {
+function buildBody( query ) {
+  return {
+    body: JSON.stringify({ query: query })
+  }
+}
+
+function buildBodyWithVariables( query, variables ) {
+  return { 
+    body: JSON.stringify({ query: query, variables: variables })
+  }
+}
+
+function buildHeaders(auth, body) {
+  return {
+
+    body: body.body,
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': body.body.length,
+        'User-Agent': '' + auth.user,
+        'Authorization': 'token ' + auth.token
+    }
+  }
+}
+
+function buildRequestOptions(request) {
+  return {
+    body: request.body,
+    options: {
+      method: 'POST',
+      host: 'api.github.com',
+      path: '/graphql',
+      port: 443,
+      headers: request.headers
+    }
+  }
+}
+
+function makeRequest(request) {
+  var options = request.options
+  var body = request.body
+
+  return new Promise(function (resolve, reject) {
+    https.request(options, function (res) {
+      var result = ''
+
+      res.on('data', function (chunk) {
+        return result += chunk
+      })
+
+      res.on('end', function () {
+        return resolve(JSON.parse(result))
+      })
+
+      res.on('error', function (err) {
+        return reject(err);
+      })
+
+    }).write(body)
+  })
+}
+
+var x = async function (filename, variables) {
   try {
-    var result = await loadQuery( './query.gql' )
-    console.log(result)
+    var query = await loadQuery( filename )
+
+    var body = buildBodyWithVariables(query, variables)
+
+    var headers = buildHeaders({
+      user: process.env.GITHUB_USERNAME, 
+      token: process.env.GITHUB_TOKEN
+    }, body)
+
+    var request = buildRequestOptions( headers )
+    var data = await makeRequest(request)
+
+    return data
+    // console.log( 'query ->\n', query)
+    // console.log( 'body ->\n', body)
+    // console.log( 'headers ->\n', headers)
+    // console.log( 'request ->\n', request)
+    // console.dir(data, {depth: null})
+
   } catch( error ) {
     console.log(error)
   }
 }
 
-x()
+x('./query.gql', {username: 'kjonathante'})
+.then( function(data) {
+  console.dir(data, {depth: null})
+})
